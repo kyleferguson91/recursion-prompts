ac9a4abeddaba414bf0fd38ab3b1c997d3bdd943
Kyleferguson91 <kyleferguson91@gmail.com>
2023-03-30 20:55:12 -0400

    more prompts done

--- src/recursion.js
+++ src/recursion.js
@@ -140,8 +140,10 @@ return n + sumBelow(n)
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+    // this prits from x+1 to y-1
+
  
-y = y - 1 
+
     // base case can return function immediately..
     // simplest case is when x == y return nothing, no range.
     if (x == y) {
@@ -149,28 +151,98 @@ y = y - 1
         // base case returns an array, from there we push each x value onto it up the chain
         return []
     }
+
     // 1 step simpler - assume it will solve this problem, 1 step closer to y!
     else {
-        
+    
+               if (x > y) {
+        y = y + 1
+        // if x is larger eg 12,8
+        // we go 11 10 9
+        // so we can post up call(9), call(10), call(11) call(12)
+        // 12 returns empty array, and 11 10 9 are posted backwards
+        // since 11 is most recent on stack, and these are 
+        // concat to the available array 
+       if (x != y) {
+        return range(x,y).concat(y)
+       }
+       // else if x is equal to y, do not concat the result, 
+       // on this step we are going to return an empty array to begin
+       // appending values, and thus we won't append the xvalue this time
+       else { return range(x,y)}
+    }        
+    
+    else if (x<y) {
+        // print up to y!
+      
+        y = y - 1
+       
+ 
+      if (x != y) {
+        return range(x,y).concat(y)
+      }
+       // else if x is equal to y, do not concat the result, 
+       // on this step we are going to return an empty array to begin
+       // appending values, and thus we won't append the xvalue this time
+      else { return range(x,y)}
+    }
+    
+        }
         
         // but we need to push this x value to an output arr.. and call the function with the 
         // array equal to the previous call result..
         // this step is the key, when we recurse, the array is equal to..
         // the current array with the result of the next call added on
-  return range(x,y).concat(y)
+
+
     }
     
  
 
-};
 
-console.log(range(2,9))
+//console.log(range(2,9))
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+
+
+
+
+    //- base case in which the function can return the result immediately
+    //- recursive case: in which the function must call itself to break the problem down to a simpler level
+    //
+    //
+
+// what is an exponent .. num x num x num...
+// whats one step simpler? num x num
+// whats the base case?  when we are left with no nums
+
+if (exp == 0) {
+    return 1
+}
+
+else {
+   if (exp > 0) {
+    return base * exponent(base, exp - 1)
+   }
+
+   else if (exp < 0) {
+    return 1 / base * exponent(base, exp + 1).toFixed(4)
+   }
+}
+
+
+    //
+    ////Break the problem I am trying to solve down into a problem that is one step simpler
+    //   // Assume that my function will work to solve the simpler problem — really believe it beyond any doubt
+    //    ask myself: since i know i can solve the simpler problem, how would i solve the more complex one 
+    //
+
+
+
 };
 
 // 8. Determine if a number is a power of two.
@@ -178,28 +250,220 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
-};
 
+//console.log(n)
+// for something to be a power of two.. 
+// 2 x 2 x 2 x 2 must eventually equal it..
+// a base case would be when the number / 2 == 0
+// if we don't reach that, then the number must not be a power.
+// one step simpler would be a number further divided by 2..
+
+// if the number divided by 2 is 1 then its a power!
+if (n  == 1) {
+    return true
+}
+// we need a base case where we get below 1 but above 0..
+else if (n < 1 && n >  0  || n == 0) {
+    console.log(n, 'n here')
+    return false
+}
+else {
+// recurse, make the problem one step simpler..
+// divide number by 2..
+return powerOfTwo(n / 2)
+
+
+}
+// return false if we get through the entire chain of calls
+// and do not return true!
+
+
+};
+//console.log(powerOfTwo(0)
+//)
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+
+
+// a base case would be a string one letter shorter
+
+// we believe that function will do work of reversing..
+
+if (string.length == 0) {
+    return string
+}
+else {
+
+    return reverse(string.slice(1)) + string[0]
+}
+
+
+
+
+
+
 };
+
+
+
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+
+ //   - base case in which the function can return the result immediately
+ //   - recursive case: in which the function must call itself to break the problem down to a simpler level
+ //   
+ //   
+ //   
+ //   //Break the problem I am trying to solve down into a problem that is one step simpler
+ //      // Assume that my function will work to solve the simpler problem — really believe it beyond any doubt
+ //       ask myself: since i know i can solve the simpler problem, how would i solve the more complex one 
+ //   
+//
+//
+// a palindrome is same forward and back..
+// we can compare 2 letters at  a time, each letter must be the same (start and end)
+// if we get a mismatch, we do not have a palindrom
+// how do we make the problem simpler..
+// what if one letter word? then we have palindrome same with zero letter
+// take a longer word.. compare first and last letter
+// if we don't have a match we don't have a palindrom
+
+if (string.length == 0 || string.length == 1) {
+    return true
+}
+
+// else we need to compare letters.. what if we have a two letter word?
+// eg aa
+
+// well if letters are equal its a palindrome..
+else if (string.length == 2) {
+    return string[0].toLowerCase() == string[string.length-1].toLowerCase()
+}
+
+// well what if 3 letters or more..
+
+else {
+    // compare first letter to last letter
+    if (string[0].toLowerCase() === string[string.length - 1].toLowerCase()) {
+        // recurse since it still may be a palindrome..
+        // but cut first and last letter off of string
+        // slice is start index and index to exclude
+
+        // so start at char 1, and start excluding at last character
+        console.log(string, string.slice(1,string.length-1))
+      return  palindrome(string.slice(1,string.length-1))
+    }
+    else {
+        // mismatch, canonot be a palindrome return false
+        return false
+    }
+}
+
+
+
+
 };
+//console.log(palindrome('bob'))
+
+
+
+
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
 // modulo(5,2) // 1
 // modulo(17,5) // 2
 // modulo(22,6) // 4
+
+// //must have:
+//   - base case in which the function can return the result immediately
+ //   - recursive case: in which the function must call itself to break the problem down to a //simpler level
+ //   
+ //   
+ //   
+ //   //Break the problem I am trying to solve down into a problem that is one step simpler
+ //      // Assume that my function will work to solve the simpler problem — really believe it //beyond any doubt
+ //       ask myself: since i know i can solve the simpler problem, how would i solve the more //complex one 
+ //   
+//
+//
+
+
+// how do we make it one step simpler..
+// reduce the divided by divisor, until we equal 0.. or cannot equal 0..
+// eg 10 / 3, 10 - 3 == 7, - 3 == 4, - 3 == 1   - 3 == - 2
+// if we reach 0, then return 0..
+// but if we dont reach 0 before we go negative, we need to return
+// the result of the call plus the divisor to get the last one back
+
+// recursive call with divided minus divisor each time..
+
+// when we have 2 negatives.. 
+// when we have one negative
+
 var modulo = function(x, y) {
+
+
+   
+
+ if (x === 0 && y === 0 ) {
+    return NaN
+}
+else if ( x === 0) {
+    return 0
+}
+else if (x > 0) {
+    
+   return modulo(x - y, y)
+}
+else if (x < 0) {
+
+return x + y
+}
+
+   
+
+
+
+
 };
+
+
+console.log(modulo(22,6))
+
+
+
+
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
+// what is multiplication, addition a certain number of times
+// adding a number 1 times is simplest, its the number..
+// thus the base case is if y == 1, then we return the number.
+// but if it isn't equal to one, we need to
+// MAKE THE PROBLEM SIMPELR 
+// well bring y one step closer to 1..
+// and return the number so we can add..
 var multiply = function(x, y) {
+if (y == 1) {
+    return x
+}
+else {
+    return x + multiply(x, y-1)
+}
+    
+
+
 };
+
+
+
+
+
+
+
+
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
